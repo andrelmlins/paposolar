4e2cf64f4efb297abcf8beddefb617917a60e422
andrelmlins <andrelucas01@hotmail.com>
2018-02-15 10:24:49 -0200

    Versão 2.0

---
+++ app/src/main/java/br/com/paposolar/Beans/Conversa.java
@@ -0,0 +1,52 @@
+package br.com.paposolar.Beans;
+
+import java.io.Serializable;
+
+/**
+ * Created by AndreLucas on 13/12/2017.
+ */
+
+public class Conversa implements Serializable{
+    private String nomeUser;
+    private String emailUser;
+    private String id;
+    private String room;
+
+    public Conversa(String nomeUser, String emailUser, String id) {
+        this.nomeUser = nomeUser;
+        this.emailUser = emailUser;
+        this.id = id;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getNomeUser() {
+        return nomeUser;
+    }
+
+    public void setNomeUser(String nomeUser) {
+        this.nomeUser = nomeUser;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/br/com/paposolar/Beans/UserType.java
@@ -0,0 +1,9 @@
+package br.com.paposolar.Beans;
+
+/**
+ * Created by AndreLucas on 13/12/2017.
+ */
+
+public enum UserType {
+    OTHER, SELF
+}
